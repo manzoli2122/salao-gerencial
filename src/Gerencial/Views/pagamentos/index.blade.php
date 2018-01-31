fcf52c59f7ef2d7a6d3ad838b4e9a64f9a6879da
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-01-31 10:02:36 -0200

    30-01-2018

--- src/Gerencial/Views/pagamentos/index.blade.php
+++ src/Gerencial/Views/pagamentos/index.blade.php
@@ -4,26 +4,18 @@
 	Listagem dos Pagamentos
 @endsection
 
-
 @section( Config::get('app.templateMasterContent' , 'content')  )
 
 <div class="col-xs-12">
     <div class="box box-success">
-
-			
-		
-		
         <div class="box-body">
             <table class="table table-bordered table-striped table-hover" id="datatable">
                 <thead>
                     <tr>
-						<th pesquisavel style="max-width:30px">ID</th>
-						
+						<th pesquisavel style="max-width:30px">ID</th>						
                         <th style="max-width:120px">Data</th>
                         <th>Forma de Pagamento</th>
-						<th>Valor</th>		
-								
-						
+						<th>Valor</th>														
                         <th class="align-center" style="width:100px">Ações</th>
                     </tr>
                 </thead>
@@ -34,42 +26,8 @@
 
 @endsection
 
-
 @push(Config::get('app.templateMasterScript' , 'script') )
 	<script src="{{ mix('js/datatables-padrao.js') }}" type="text/javascript"></script>
-
-	<script>
-				
-		window.confirmarOperadoraPagamentoPeloId = function(id, texto, url, funcSucesso = function() {}) {
-			alertConfimacao(texto, '',
-				function() {
-					alertProcessando();
-					var token = document.head.querySelector('meta[name="csrf-token"]').content;
-
-					$.ajax({
-						url: url + "/" + id,
-						type: 'post',
-						data: {  _token: token },
-						success: function(retorno) {
-							alertProcessandoHide();
-							if (retorno.erro) {
-								toastErro(retorno.msg);
-							} else {
-								toastSucesso(retorno.msg);
-								funcSucesso();
-							}
-						},
-						error: function(erro) {
-							alertProcessandoHide();
-							toastErro("Ocorreu um erro");
-							console.log(erro);
-						}
-					});
-				}
-			);
-		}
-	</script>
-
 	<script>
 		$(document).ready(function() {
 			var dataTable = datatablePadrao('#datatable', {
