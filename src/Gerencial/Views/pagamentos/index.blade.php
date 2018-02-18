e5e9090ef607d559f193e9b4ff718f58d542efbf
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-18 20:34:53 -0300

    30-01-2018

--- src/Gerencial/Views/pagamentos/index.blade.php
+++ src/Gerencial/Views/pagamentos/index.blade.php
@@ -31,8 +31,8 @@
 						<th pesquisavel>Bandeira</th>
 						<th pesquisavel>Cliente</th>
 						<th>Valor</th>			
-						<th pesquisavel style="max-width:30px">V.P.O.</th>	
-						<th pesquisavel style="max-width:40px">Na Conta</th>										
+						<th pesquisavel style="max-width:40px">V.P.O.</th>	
+						<th pesquisavel style="max-width:50px">Na Conta</th>										
 						<th class="align-center" style="width:100px">Ação</th>
 						
                     </tr>
